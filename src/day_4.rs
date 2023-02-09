1f51ef1915658fc29ed8579b5e83f6b2d1c95e0b
Obl4ki <lukaszoblak33@gmail.com>
2023-02-09 20:22:27 +0100

    Day 4 part 2

--- src/day_4.rs
+++ src/day_4.rs
@@ -37,6 +37,33 @@ create_day!(
         println!("{}", &counts)
     },
     |file_content: String| {
-        todo!();
+        let pairs_iter = file_content
+            .lines()
+            .map(|line| line.split(',').collect_tuple::<(_, _)>())
+            .map(Option::unwrap);
+
+        let split_parse_elf = |elf: String| -> (u32, u32) {
+            elf.split('-')
+                .map(str::parse::<u32>)
+                .map(Result::unwrap)
+                .collect_tuple::<(_, _)>()
+                .unwrap()
+        };
+
+        let counts: usize = pairs_iter
+            .map(|(first, second)| {
+                let (first_start, first_end) = split_parse_elf(first.to_owned());
+                let (second_start, second_end) = split_parse_elf(second.to_owned());
+
+                vec![(first_start, first_end), (second_start, second_end)]
+                    .into_iter()
+                    .sorted_by(|(x1, _), (x2, _)| x1.cmp(x2))
+                    .collect_tuple::<((u32, u32), (u32, u32))>()
+                    .unwrap()
+            })
+            .filter(|((_, first_end), (second_start, _))| first_end >= second_start)
+            .count();
+
+        println!("{}", &counts)
     }
 );
