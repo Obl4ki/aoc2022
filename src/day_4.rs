f677641a6efcf2646d8d5bc6342cb8ec73c69104
Obl4ki <lukaszoblak33@gmail.com>
2023-02-09 20:18:21 +0100

    Refactor day 4

--- src/day_4.rs
+++ src/day_4.rs
@@ -5,42 +5,38 @@ use crate::create_day;
 create_day!(
     '4',
     |file_content: String| {
-        let output: u32 = file_content
+        let pairs_iter = file_content
             .lines()
             .map(|line| line.split(',').collect_tuple::<(_, _)>())
-            .map(Option::unwrap)
-            .map(|(first, second)| {
-                let split_parse_elf = |elf: String| -> (u32, u32) {
-                    elf.split('-')
-                        .map(str::parse::<u32>)
-                        .map(Result::unwrap)
-                        .collect_tuple::<(_,_)>()
-                        .unwrap()
-                };
+            .map(Option::unwrap);
 
+        let split_parse_elf = |elf: String| -> (u32, u32) {
+            elf.split('-')
+                .map(str::parse::<u32>)
+                .map(Result::unwrap)
+                .collect_tuple::<(_, _)>()
+                .unwrap()
+        };
+
+        let counts: usize = pairs_iter
+            .map(|(first, second)| {
                 let (first_start, first_end) = split_parse_elf(first.to_owned());
                 let (second_start, second_end) = split_parse_elf(second.to_owned());
 
-                let (first_start, second_start, first_end, second_end) =
-                    if first_start < second_start {
-                        (first_start, second_start, first_end, second_end)
-                    } else {
-                        (second_start, first_start, second_end, first_end)
-                    };
-
-                if first_end >= second_end || first_start == second_start
-                // || first_start < second_start && first_end < second_end
-                {
-                    println!("T {first_start}-{first_end}, {second_start}-{second_end}");
-                    1
-                } else {
-                    println!("X {first_start}-{first_end}, {second_start}-{second_end}");
-                    0
-                }
+                vec![(first_start, first_end), (second_start, second_end)]
+                    .into_iter()
+                    .sorted_by(|(x1, _), (x2, _)| x1.cmp(x2))
+                    .collect_tuple::<((u32, u32), (u32, u32))>()
+                    .unwrap()
             })
-            .sum();
+            .filter(|((first_start, first_end), (second_start, second_end))| {
+                first_end >= second_end || first_start == second_start
+            })
+            .count();
 
-        println!("{output}")
+        println!("{}", &counts)
     },
-    |file_content: String| { todo!() }
+    |file_content: String| {
+        todo!();
+    }
 );
